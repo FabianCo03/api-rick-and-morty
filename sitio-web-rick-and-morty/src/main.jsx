6128182dbaaf9de45eb6fbfd82bd41df4bbb0962
Fabianormo <fabianormo@outlook.com>
2024-04-25 10:48:26 -0500

    feat: 🎸 Add prettier and some instructions

--- sitio-web-rick-and-morty/src/main.jsx
+++ sitio-web-rick-and-morty/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Punto de entrada de la App
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
