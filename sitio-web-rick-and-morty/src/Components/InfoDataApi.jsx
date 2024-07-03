1b553d8b6a743a9f75b23f3f90ec313db2259ea0
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro-2.local>
2024-07-03 11:37:37 -0500

    feat: 🎸 Change text in infoDataApi

--- sitio-web-rick-and-morty/src/Components/InfoDataApi.jsx
+++ sitio-web-rick-and-morty/src/Components/InfoDataApi.jsx
@@ -6,7 +6,7 @@ const InfoDataApi = ({ data }) => {
     >
       <div className="flex justify-center p-8">
         <h2 className="text-3xl font-bold text-white bg-gradient-to-r from-blue-900 to-green-900 px-4 py-2 rounded">
-          Datos encontrados: {data.length}
+          Datos de la API: {data.length}
         </h2>
       </div>
 
